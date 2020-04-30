88b36d1101186a40c60a10b579f7b1670082e9bf
leejhawkins <59981819+leejhawkins@users.noreply.github.com>
2020-04-30 15:56:41 -0600

    initial commit, file structure

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers (
+    id int NOT NULL auto_increment,
+    burger_name varchar(200) NOT NULL,
+    devoured BOOLEAN default false,
+    PRIMARY KEY (id)
+)
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) values ('BBQ Bacon Burger');
+INSERT INTO burgers (burger_name) values ('California Burger');
+INSERT INTO burgers (burger_name) values ('Veggie Burger');
+INSERT INTO burgers (burger_name) values ('Black Bean Burger');
