0d525b5466fba36bc0a1fe175529727e499ebed0
leejhawkins <59981819+leejhawkins@users.noreply.github.com>
2020-05-01 17:34:39 -0600

    finished page

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 INSERT INTO burgers (burger_name) values ('BBQ Bacon Burger');
 INSERT INTO burgers (burger_name) values ('California Burger');
-INSERT INTO burgers (burger_name,devoured) values ('Veggie Burger',true);
-INSERT INTO burgers (burger_name,devoured) values ('Black Bean Burger',true);
+INSERT INTO burgers (burger_name) values ('Triple Big Mac');
+INSERT INTO burgers (burger_name) values ('Impossible Whopper');
